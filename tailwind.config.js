54420b8fcf6fe4c9787dc075634accaac1879c6f
YasTokan <y.tokan.95@gmail.com>
2025-05-29 20:54:44 +0400

    mostly done updating styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,18 +40,42 @@ module.exports = {
         arabic: ['AvenirArabic-SharjahGovt', 'sans-serif'] // For Arabic
       },
       fontSize: {
-        // Desktop Sizes
-        h1: ['53.3px', { lineHeight: '1.2' }], // 40pt
-        h2: ['56px', { lineHeight: '1.2' }],   // 42pt
-        h3: ['48px', { lineHeight: '1.2' }],   // 36pt
-        h4: ['34.6px', { lineHeight: '1.2' }], // 26pt
-        h5: ['26.6px', { lineHeight: '1.2' }], // 20pt
-        body: ['18.6px', { lineHeight: '1.6' }], // 14pt
-        'l-body': ['21.3px', { lineHeight: '1.6' }], // 16pt
-        's-body': ['16px', { lineHeight: '1.5' }], // 12pt
-        subtitle: ['21.3px', { lineHeight: '1.4' }], // 16pt
-        subhead: ['24px', { lineHeight: '1.4' }] // 18pt
-      },
+        h1: ['40px', { lineHeight: '1.2' }],
+        'h1-mobile': ['24px', { lineHeight: '1.2' }],
+        h2: ['42px', { lineHeight: '1.2' }],
+        'h2-mobile': ['28px', { lineHeight: '1.2' }],
+        h3: ['36px', { lineHeight: '1.2' }],
+        'h3-mobile': ['24px', { lineHeight: '1.2' }],
+        h4: ['26px', { lineHeight: '1.2' }],
+        'h4-mobile': ['24px', { lineHeight: '1.2' }],
+        h5: ['20px', { lineHeight: '1.2' }],
+        'h5-mobile': ['16px', { lineHeight: '1.2' }],
+        body: ['14px', { lineHeight: '1.6' }],
+        'body-l': ['16px', { lineHeight: '1.6' }],
+        'body-s': ['12px', { lineHeight: '1.5' }],
+        subtitle: ['16px', { lineHeight: '1.4' }],
+        subhead: ['18px', { lineHeight: '1.4' }],
+        'subhead-mobile': ['14px', { lineHeight: '1.4' }],
+      }
+      ,
+      /*  fontSize: {
+         h1: ['53px', { lineHeight: '1.2' }],
+         'h1-mobile': ['32px', { lineHeight: '1.2' }],
+         h2: ['56px', { lineHeight: '1.2' }],
+         'h2-mobile': ['37px', { lineHeight: '1.2' }],
+         h3: ['36px', { lineHeight: '1.2' }],
+         'h3-mobile': ['32px', { lineHeight: '1.2' }],
+         h4: ['35px', { lineHeight: '1.2' }],
+         'h4-mobile': ['32px', { lineHeight: '1.2' }],
+         h5: ['27px', { lineHeight: '1.2' }],
+         'h5-mobile': ['21px', { lineHeight: '1.2' }],
+         body: ['19px', { lineHeight: '1.6' }],
+         'body-l': ['21px', { lineHeight: '1.6' }],
+         'body-s': ['16px', { lineHeight: '1.5' }],
+         subtitle: ['21px', { lineHeight: '1.4' }],
+         subhead: ['24px', { lineHeight: '1.4' }],
+         'subhead-mobile': ['19px', { lineHeight: '1.4' }],
+       }, */
       keyframes: {
         'fade-in-up': {
           '0%': { opacity: 0, transform: 'translateY(20px)' },
